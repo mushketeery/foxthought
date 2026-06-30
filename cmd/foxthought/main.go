d84a8d3398f3c6ee6d96c13a39ece517f7da8b33
Mushy Hyena <mushketeery@gmail.com>
2026-06-30 19:40:20 +0000

    feat: Add functions for validation args length and extracting argument

--- cmd/foxthought/main.go
+++ cmd/foxthought/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Oofity Loofity!")
+	arguments := os.Args[1:] // os.Args[0] is the program name
+
+	err := validateArgsLength(arguments)
+	if err != nil {
+		fmt.Printf("\u001B[33m[Error]\u001B[0m %v\n", err)
+		os.Exit(1)
+	}
+
+	thought := extractThought(arguments)
+	fmt.Println(thought)
+}
+
+// validateArgsLength checks that the number of arguments is not greater than 1.
+func validateArgsLength(args []string) error {
+	if len(args) > 1 {
+		return fmt.Errorf("Too many thoughts...")
+	}
+
+	return nil
+}
+
+// extractThought returns the first argument string if it exists, else defaulting.
+func extractThought(args []string) string {
+	if len(args) == 0 {
+		return "Oofity Loofity!"
+	}
+
+	return args[0]
 }
