457508645c22fc741c5dda420f3921862a3c8f2f
Mushy Hyena <mushketeery@gmail.com>
2026-06-30 20:56:02 +0000

    feat: Add and configure Nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# https://wiki.nixos.org/wiki/Go
+
+{
+  description = "foxthought: cowsay but it's a fox and his thoughts";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forAllSystems (
+        { pkgs }: {
+          default = pkgs.buildGoModule rec {
+            pname = "foxthought";
+            version = "1.0.0";
+            subPackages = [ "cmd/foxthought" ];
+            src = ./.;
+            vendorHash = null;
+
+            meta = {
+              description = "foxthought: cowsay but it's a fox and his thoughts";
+              homepage = "https://github.com/mushketeery/foxthought";
+              license = pkgs.lib.licenses.gpl3Only;
+              mainProgram = "foxthought";
+            };
+          };
+        }
+      );
+    };
+}
