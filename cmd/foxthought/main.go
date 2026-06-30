18f47f3b26a1ca7ae97d6a2508d5432e2c0c109e
Mushy Hyena <mushketeery@gmail.com>
2026-06-30 20:32:04 +0000

    feat: Wire up program

--- cmd/foxthought/main.go
+++ cmd/foxthought/main.go
@@ -6,32 +6,20 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:] // os.Args[0] is the program name
+	// os.Args[0] is the program name, so get proper arguments
+	arguments := os.Args[1:]
 
+	// check for correct number of arguments
 	err := validateArgsLength(arguments)
 	if err != nil {
 		fmt.Printf("\u001B[33m[Error]\u001B[0m %v\n", err)
 		os.Exit(1)
 	}
 
-	thought := extractThought(arguments)
+	// get argument string and wrap in bubble (defaults to "Oofity Loofity!")
+	thought := wrapThought(extractThought(arguments, "Oofity Loofity!"))
+
+	// print thought and fox
 	fmt.Println(thought)
-}
-
-// validateArgsLength checks that the number of arguments is not greater than 1.
-func validateArgsLength(args []string) error {
-	if len(args) > 1 {
-		return fmt.Errorf("Too many thoughts...")
-	}
-
-	return nil
-}
-
-// extractThought returns the first argument string if it exists, else defaulting.
-func extractThought(args []string) string {
-	if len(args) == 0 {
-		return "Oofity Loofity!"
-	}
-
-	return args[0]
+	fmt.Println(fox)
 }
